cacda768d816c3cd9e1d9d461d14530d2345f180
Christian König <koenig.kersting@gmail.com>
2017-08-11 15:12:42 +0200

    added chat

---
+++ chat_example/views.py
@@ -0,0 +1,18 @@
+from otree.api import Currency as c, currency_range
+from . import models
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+
+class Chat(Page):
+    pass
+
+
+class Results(Page):
+    pass
+
+
+page_sequence = [
+    Chat,
+    Results
+]
